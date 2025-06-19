b4f86dc718ef3379379cd8a5eef7fcd2d48cfd74
Vemulapalli Sri Samadarsini <167125558+samadarsini@users.noreply.github.com>
2025-06-19 19:02:18 -0400

    Update register.sv

--- lab1/register.sv
+++ lab1/register.sv
@@ -2,13 +2,13 @@ module register(out,clk,enable,rst_,data);
 input logic [7:0]data;
 input logic clk, enable, rst_;
 output logic [7:0]out;
+  
 timeunit 1ns;
 timeprecision 100ps;
-always_ff @(posedge clk or negedge rst_)
-begin
-if(!rst_)
-out<=8'b0;
-else if(enable)
-out<=data;
-end
+  
+  always_ff @(posedge clk, negedge rst_)
+    if(!rst_)
+      out<=0;
+    else if(enable)
+      out<=data;
 endmodule
